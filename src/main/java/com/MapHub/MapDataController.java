777ff7508b91937778a1d21502fca7ddebc53861
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-09-29 11:38:36 +0100

    added backend params

--- src/main/java/com/MapHub/MapDataController.java
+++ src/main/java/com/MapHub/MapDataController.java
@@ -2,10 +2,7 @@ package com.MapHub;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -16,7 +13,9 @@ import java.util.List;
 public class MapDataController {
 
     @GetMapping("/api/getPolygon")
-    public ResponseEntity<?> getPolygon() {
+    public ResponseEntity<?> getPolygon(@RequestParam("bounds") double[] mapbounds,@RequestParam("zoom") double zoom) {
+        System.out.println(mapbounds);
+        System.out.println(zoom);
         LatLng[] bounds = {new LatLng(70,60), new LatLng(70,70), new LatLng(60,70), new LatLng(60,60),new LatLng(70,60)};
         ProgressArea a = new ProgressArea("#777777",new ArrayList<>(List.of(bounds)),1,"test","i hope this works",new ArrayList<>(List.of(new String[]{"leopardm"})) );
 
