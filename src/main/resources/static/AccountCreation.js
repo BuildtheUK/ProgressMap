0607072d6603c0cc4b3d5f28d944f9ded159f329
Alex <aym1g24@soton.ac.uk>
2026-06-09 01:56:14 +0100

    Show password, ability to click on and off markers to show and hide information

--- src/main/resources/static/AccountCreation.js
+++ src/main/resources/static/AccountCreation.js
@@ -4,6 +4,35 @@ const passwordInput = document.getElementById('passwordInput')
 const repeatPasswordInput = document.getElementById('repeatPasswordInput')
 const errorMessage = document.getElementById('errorMessage')
 const otcInput = document.getElementById('codeInput')
+const showPassword = document.getElementById('showPassword')
+const showRepeatPassword = document.getElementById('showRepeatPassword')
+
+const eyeOffSVG = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M792-56 624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM480-320q11 0 20.5-1t20.5-4L305-541q-3 11-4 20.5t-1 20.5q0 75 52.5 127.5T480-320Zm292 18L645-428q7-17 11-34.5t4-37.5q0-75-52.5-127.5T480-680q-20 0-37.5 4T408-664L306-766q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302ZM587-486 467-606q28-5 51.5 4.5T559-574q17 18 24.5 41.5T587-486Z"/></svg>`
+const eyeOnSVG = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M607.5-372.5Q660-425 660-500t-52.5-127.5Q555-680 480-680t-127.5 52.5Q300-575 300-500t52.5 127.5Q405-320 480-320t127.5-52.5Zm-204-51Q372-455 372-500t31.5-76.5Q435-608 480-608t76.5 31.5Q588-545 588-500t-31.5 76.5Q525-392 480-392t-76.5-31.5ZM214-281.5Q94-363 40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200q-146 0-266-81.5Z"/></svg>`
+
+if(showPassword != null) {
+    showPassword.addEventListener("click", () => {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            showPassword.innerHTML = eyeOffSVG
+        } else {
+            passwordInput.type = "password";
+            showPassword.innerHTML = eyeOnSVG
+        }
+    })
+}
+
+if(showRepeatPassword != null) {
+    showRepeatPassword.addEventListener("click", () => {
+        if (repeatPasswordInput.type === "password") {
+            repeatPasswordInput.type = "text";
+            showRepeatPassword.innerHTML = eyeOffSVG
+        } else {
+            repeatPasswordInput.type = "password";
+            showRepeatPassword.innerHTML = eyeOnSVG
+        }
+    })
+}
 
 form.addEventListener('submit', (e) => {
 
--- src/main/resources/static/homePage.js
+++ src/main/resources/static/homePage.js
@@ -1,5 +1,19 @@
-const loginButton = document.getElementById("btnLogin")
+const map = L.map('map').setView([51.505, -0.09], 13);
 
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const loginButton = document.getElementById("btnLogin")
+const regionInfoBox = document.getElementById("RegionInfoBox")
+const buildingInfoBox = document.getElementById("BuildingInfoBox")
+const noItemSelectedInfoBox = document.getElementById("NoItemSelected")
+const buildingName = document.getElementById("BuildingName")
+const buildingBuilder = document.getElementById("BuildingBuilder")
+const buildingCreationDate = document.getElementById("BuildingCreationDate")
+var loggedIn = false;
+var username = ""
 window.addEventListener("load", () => {
     fetch("/user/me", {
         method: "GET",
@@ -8,6 +22,7 @@ window.addEventListener("load", () => {
         .then(async (res) => {
 
             if (!res.ok) {
+                loggedIn = false
                 loggedOutDisplay();
                 return;
             }
@@ -16,9 +31,13 @@ window.addEventListener("load", () => {
 
 
             if (!data.loggedIn) {
+                loggedIn = false
+
                 loggedOutDisplay();
                 return;
             }
+            username = data.username
+            loggedIn = true
 
             loggedInDisplay(data.username);
 
@@ -26,12 +45,44 @@ window.addEventListener("load", () => {
         })
         .catch(err => {
             console.log(err);
+            loggedIn = false
+
             loggedOutDisplay();
         });
+    var marker = L.marker([55, 0], {buildingId: 1}).addTo(map);
+    marker.on('click',(e) => displayBuildingBox(e.target.options.id))
+    map.on('click', displayWelcomeBox)
 });
+
+function displayWelcomeBox(){
+    buildingInfoBox.style.display = "none"
+    regionInfoBox.style.display = "none"
+
+    if(loggedIn){
+        noItemSelectedInfoBox.children.item(0).innerHTML = "Hello, " + username + "! Welcome back to BTUK Progress Map. <br> Explore our current progress or create and edit your own claims!"
+    }
+    else{
+        noItemSelectedInfoBox.children.item(0).innerHTML = "Welcome to BTUK Progress Map! <br> Have a look around or create an account to link to your in-game progress"
+    }
+    noItemSelectedInfoBox.style.display = "flex"
+}
+
+
+function displayBuildingBox(buildingId){
+    const builder = "Poole"
+    const dateAdded = "07/06/2026"
+    const name = "test Building"
+    noItemSelectedInfoBox.style.display = "none"
+    buildingInfoBox.style.display = "flex"
+    buildingName.value = name
+    buildingBuilder.innerText = "Builder: " + builder
+    buildingCreationDate.innerText = "Created on: " + dateAdded
+}
 
 function loggedInDisplay(username){
     document.getElementById("MCSkinLogo").src = "https://mc-heads.net/avatar/" + username
+    noItemSelectedInfoBox.style.display = "flex";
+    displayWelcomeBox()
     loginButton.innerText = "Logout"
     loginButton.onclick = ( () => {
         fetch("/logout", {method: "POST", credentials: "include"}).then(() => window.location.reload()).catch(err => console.log(err))
@@ -40,6 +91,8 @@ function loggedInDisplay(username){
 
 function loggedOutDisplay(){
     document.getElementById("MCSkinLogo").src = "images/blankProfile.svg"
+    noItemSelectedInfoBox.style.display = "flex";
+    displayWelcomeBox()
     loginButton.innerText = "Login"
     loginButton.onclick = ( () => {
         window.location.assign("login.html")
