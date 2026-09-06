53a42aeac500e899d71a26afded85a93e4a7120b
Alex <aym1g24@soton.ac.uk>
2026-09-06 17:16:44 +0100

    started file loading code

--- src/main/java/com/example/maphub/services/OldPolygonAPIService.java
+++ src/main/java/com/example/maphub/services/OldPolygonAPIService.java
@@ -6,6 +6,13 @@ import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Polygon;
 import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.stereotype.Service;
+import tools.jackson.core.JacksonException;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.System;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +20,7 @@ import java.util.List;
 @Service
 public class OldPolygonAPIService implements PolygonAPI {
     private final GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
+    private final String directory = System.getProperty("user.dir");
     public List<ProgressArea> getPolygons(double minLat, double minLon, double maxLat, double maxLon, String uuid) {
             Coordinate[] coordinates = new Coordinate[] {
                     new Coordinate(-2.395048498111797, 51.355557375227946),
@@ -36,4 +44,36 @@ public class OldPolygonAPIService implements PolygonAPI {
             polygons.add(a);
             return polygons;
         }
+
+        //load a list of all progress areas into an in memory array
+        //this array is then used to find the areas that overlap with the map view
+    //hopefully efficiently although this is not a permenant feature as progress manager will
+    //replace it
+    //I need to actually change a lot here but this should be rough structure
+        private void loadFile(){
+            File myObj = new File("filename.txt");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode jsonNode = objectMapper.readTree(myObj);
+            String name = jsonNode.get("name").asText();
+            int age = jsonNode.get("age").asInt();
+            String city = jsonNode.get("city").asText();
+            String state = jsonNode.get("state").asText();
+            String country = jsonNode.get("country").asText();
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("City: " + city);
+            System.out.println("State: " + state);
+            System.out.println("Country: " + country);
+
+
+        }catch (JacksonException e) {
+            System.out.println("Progress Area file not loaded");
+        }
+
+            // try-with-resources: Scanner will be closed automatically
+
+
+
+    }
     }
