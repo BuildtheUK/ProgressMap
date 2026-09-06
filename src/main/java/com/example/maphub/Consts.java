7ee5ad763a554a29fd2e19ad8f359cc6acb459e8
Alex <aym1g24@soton.ac.uk>
2026-09-06 17:32:44 +0100

    made work with seconds as time played instead of millis

--- src/main/java/com/example/maphub/Consts.java
+++ src/main/java/com/example/maphub/Consts.java
@@ -5,5 +5,5 @@ import java.time.LocalDateTime;
 public class Consts {
     public static final LocalDateTime SERVER_START_TIME = LocalDateTime.of(2020, 3,29,22,37);
     public static final int TOTAL_UK_BUILDINGS = 30000000;
-    public static final int MILLIS_IN_DAY = 86400000;
+    public static final int SECONDS_IN_DAY = 86400;
 }
